63a2e8fde9e78e7e684d70745a93eb792a08e81a
Merijn Bol <merijn@merijnbol.nl>
2020-04-28 11:24:12 +0200

    Fix the command for running detatched.

--- docs/deploy-script.bash
+++ docs/deploy-script.bash
@@ -14,6 +14,6 @@ rm -rf meteor_app.tar.gz
 # install npm packages
 cd bundle/programs/server && npm install
 
-# deploy app
+# deploy app, run as background with nohup. Need better solution. (.service)
 cd /srv/meteor_build/bundle
-MONGO_URL=mongodb://localhost:27017/hikisu_1 ROOT_URL=http://localhost PORT=3000 nohup node main.js
+MONGO_URL=mongodb://127.0.0.1:27017/hikisu_1 ROOT_URL=http://127.0.0.1 PORT=3000 nohup node main.js &
